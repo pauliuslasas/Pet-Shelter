c454968e267587adc61e352ac0860f553b3bca40
pauliuslasas <46401887+pauliuslasas@users.noreply.github.com>
2021-05-11 18:17:32 +0300

    Update settings.py

--- Samanos/settings.py
+++ Samanos/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(e+0q_9b#zj64)&t$#xdf36^_@qfu+5zkvq$*@%z60@fzqnsrz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
